189786d3bd1da09aa438d9008c658598648cba62
KeiLee <klee3309@icloud.com>
2019-09-09 13:28:26 +0900

    パッケージ３つ作ったよ

---
+++ myInterface/Shape.java
@@ -0,0 +1,20 @@
+package myInterface;
+
+
+/**
+ * インターフェース Shape のための注釈をここに書きます.
+ * 
+ * @author (あなたの名前) 
+ * @version (バージョン番号もしくは日付)
+ */
+
+public interface Shape
+{
+    /**
+     * メソッドのヘッダ例 - あなた自身のコメントで置き換えてください
+     * 
+     * @param  y    メソッドのためのサンプル引数
+     * @return        sampleMethod の戻り値
+     */
+    int sampleMethod(int y);
+}
